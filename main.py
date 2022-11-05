960beedf4c4d156c78f88e5c20c50062c22b29f5
Jason Washburn <jason@shyftsolutions.io>
2022-11-05 23:30:59 +0000

    refactored out parameter retrieval

--- main.py
+++ main.py
@@ -1,30 +1,81 @@
 """Initial test script used in development."""
-
 import datetime as dt
+from dataclasses import dataclass
+from datetime import datetime
 
 import xarray as xr
+from numpy.typing import ArrayLike
+from xarray.core.types import InterpOptions
 
-desired_locations = [{"name": "Home", "lat": 41.09, "lon": -95.94}]
 
-desired_params = [{"key": "tmp2m"}, {"key": "rh2m"}, {"key": "gustsfc"}]
+@dataclass
+class Location:
+    """Dataclass used to store a Location."""
+
+    name: str
+    latitude: float
+    longitude: float
+
+
+@dataclass
+class Parameter:
+    """Dataclass used to store weather parameter info."""
+
+    name: str
+    key: str
+
+
+desired_locations = [Location(name="Home", latitude=41.09, longitude=-95.94)]
+
+desired_params = [
+    Parameter(name="temperature", key="tmp2m"),
+    Parameter(name="relative humidity", key="rh2m"),
+    Parameter(name="wind speed (gust)", key="gustsfc"),
+]
+
+
+def get_parameter_value(
+    parameter: Parameter,
+    location: Location,
+    valid_time: datetime,
+    method: InterpOptions = "linear",
+) -> ArrayLike:
+    """Returns the value of a specific weather parameter at a provided location and time.
+
+    Args:
+        parameter (Parameter): The requested weather parameter.
+        location (Location): The requested location.
+        valid_time (datetime): The requested time.
+        method (InterpOptions, optional): Method to use for Interpolation. Defaults to "linear".
+
+    Returns:
+        ArrayLike: The value of the requested weather parameter
+    """
+    gfs_link = "http://nomads.ncep.noaa.gov:80/dods/gfs_0p25/gfs20221105/gfs_0p25_12z"
+    ds = xr.open_dataset(gfs_link, use_cftime=True)
+
+    value = (
+        ds[parameter.key]
+        .sel(
+            lat=location.latitude,
+            lon=location.longitude,
+            time=valid_time,
+            method="nearest",
+        )
+        .data
+    )
+
+    return value
 
 
 def main() -> None:
     """Main function used for testing."""
-    gfs_link = "http://nomads.ncep.noaa.gov:80/dods/gfs_0p25/gfs20221105/gfs_0p25_12z"
-
-    ds = xr.open_dataset(gfs_link)
-
     for location in desired_locations:
-        lat = location.get("lat")
-        lon = location.get("lon")
         time = dt.datetime.utcnow()
-        for param in desired_params:
+        print(f"{location.name}: {time.isoformat()}")
+        for parameter in desired_params:
             print(
-                f"{param}: ",
-                ds[param.get("key")]
-                .sel(lat=lat, lon=lon, time=time, method="nearest")
-                .data,
+                f"{parameter.name}: {get_parameter_value(parameter=parameter, location=location, valid_time=time)}"
             )
 
 
